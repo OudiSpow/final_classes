6ff04c5d6f8f2dfb1ca8c4163fc22fafb32726cf
OudiSpow <123125495+OudiSpow@users.noreply.github.com>
2023-10-30 14:19:41 +0100

    Add files via upload

---
+++ Math3.java
@@ -0,0 +1,26 @@
+package les12;
+
+public class Math3 {
+    public final static int getSum(int i, int i2, int i3) {
+        return i + i2 + i3;
+    }
+
+    public final static int getResult(int i, int i2, int i3) {
+        return i * i2 * i3;
+    }
+
+    public final static int getMax(int i1, int i2, int i3) {
+        int max = i1;
+
+        if (i2 > max)
+            max = i2;
+        if (i3 > max)
+            max = i3;
+
+        return max;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getMax(78, 79, 89));
+    }
+}
